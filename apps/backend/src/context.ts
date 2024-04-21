af1a7af2976af781c6a1161613148fa97ca8d9eb
Aplulu <aplulu.liv@gmail.com>
2024-04-22 08:09:49 +0900

    :+1: format修正

--- apps/backend/src/context.ts
+++ apps/backend/src/context.ts
@@ -2,8 +2,8 @@ import { createContextKey } from '@connectrpc/connect';
 
 export const kVODURL = createContextKey<string | undefined>(undefined);
 export const kCloudFrontPrivateKey = createContextKey<string | undefined>(
-	undefined
+  undefined
 );
 export const kCloudFrontKeyPairID = createContextKey<string | undefined>(
-	undefined
+  undefined
 );
--- apps/backend/src/handler.ts
+++ apps/backend/src/handler.ts
@@ -1,63 +1,63 @@
 import {
-	type ConnectRouter,
-	type ConnectRouterOptions,
-	ContextValues,
-	createConnectRouter,
+  type ConnectRouter,
+  type ConnectRouterOptions,
+  ContextValues,
+  createConnectRouter,
 } from '@connectrpc/connect';
 import {
-	UniversalHandler,
-	universalServerRequestFromFetch,
-	universalServerResponseToFetch,
+  UniversalHandler,
+  universalServerRequestFromFetch,
+  universalServerResponseToFetch,
 } from '@connectrpc/connect/protocol';
 import { cors } from '@nyantube/cors';
 
 interface WorkerHandlerOptions<Env> extends ConnectRouterOptions {
-	routes: (router: ConnectRouter) => void;
-	contextValues?: (
-		req: Request,
-		env: Env,
-		ctx: ExecutionContext
-	) => ContextValues;
+  routes: (router: ConnectRouter) => void;
+  contextValues?: (
+    req: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ) => ContextValues;
 }
 
 export const createWorkerHandler = <Env>(
-	options: WorkerHandlerOptions<Env>
+  options: WorkerHandlerOptions<Env>
 ) => {
-	const router = createConnectRouter();
-	options.routes(router);
+  const router = createConnectRouter();
+  options.routes(router);
 
-	const paths = new Map<string, UniversalHandler>();
-	for (const uh of router.handlers) {
-		paths.set(uh.requestPath, uh);
-	}
+  const paths = new Map<string, UniversalHandler>();
+  for (const uh of router.handlers) {
+    paths.set(uh.requestPath, uh);
+  }
 
-	return {
-		fetch: cors(
-			{
-				allowedOrigins: ['*'],
-				allowedHeaders: ['Content-Type', 'Connect-Protocol-Version'],
-			},
-			async (req: Request, env: Env, ctx: ExecutionContext) => {
-				const url = new URL(req.url);
+  return {
+    fetch: cors(
+      {
+        allowedOrigins: ['*'],
+        allowedHeaders: ['Content-Type', 'Connect-Protocol-Version'],
+      },
+      async (req: Request, env: Env, ctx: ExecutionContext) => {
+        const url = new URL(req.url);
 
-				const handler = paths.get(url.pathname);
-				if (!handler) {
-					return new Response('Not found', { status: 404 });
-				}
+        const handler = paths.get(url.pathname);
+        if (!handler) {
+          return new Response('Not found', { status: 404 });
+        }
 
-				try {
-					const uReq = {
-						...universalServerRequestFromFetch(req, {}),
-						contextValues: options.contextValues?.(req, env, ctx),
-					};
+        try {
+          const uReq = {
+            ...universalServerRequestFromFetch(req, {}),
+            contextValues: options.contextValues?.(req, env, ctx),
+          };
 
-					const uRes = await handler(uReq);
-					return universalServerResponseToFetch(uRes);
-				} catch (err) {
-					console.error(err);
-					return new Response('Internal server error', { status: 500 });
-				}
-			}
-		),
-	};
+          const uRes = await handler(uReq);
+          return universalServerResponseToFetch(uRes);
+        } catch (err) {
+          console.error(err);
+          return new Response('Internal server error', { status: 500 });
+        }
+      }
+    ),
+  };
 };
